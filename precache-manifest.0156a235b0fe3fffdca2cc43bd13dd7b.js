5139d0efd837b459ee4be828d74e9bd702c2e484
txh <txh120530@example.com>
2020-01-13 18:21:04 -0600

    Updates

---
+++ precache-manifest.0156a235b0fe3fffdca2cc43bd13dd7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907fcb709c9c867bac6b68401b7a8e41",
+    "url": "/react-meme-generator/index.html"
+  },
+  {
+    "revision": "ee3f637437e5b5e2e9ca",
+    "url": "/react-meme-generator/static/css/main.ee4795c7.chunk.css"
+  },
+  {
+    "revision": "70a77699e896b41e0a74",
+    "url": "/react-meme-generator/static/js/2.0fe7fefe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-meme-generator/static/js/2.0fe7fefe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee3f637437e5b5e2e9ca",
+    "url": "/react-meme-generator/static/js/main.c97ea3d7.chunk.js"
+  },
+  {
+    "revision": "c463e6d0fb1cb699a9da",
+    "url": "/react-meme-generator/static/js/runtime-main.25d5aafa.js"
+  }
+]);
